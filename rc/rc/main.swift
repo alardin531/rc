073ccba8deab90a8cdfbaf56eccf0246c582443c
s20181102934 <s20181102934@admindeiMac-38.local>
2019-09-06 09:37:42 +0800

    admin

---
+++ rc/rc/main.swift
@@ -0,0 +1,39 @@
+//
+//  main.swift
+//  rc
+//
+//  Created by s20181102934 on 2019/9/6.
+//  Copyright © 2019 win. All rights reserved.
+//
+
+
+//
+//  main.swift
+//  sd
+//
+//  Created by s20181102934 on 2019/9/4.
+//  Copyright © 2019 win. All rights reserved.
+//
+
+var a:String
+var b:String
+print("enter username:\n")
+a=readLine()!
+if(a=="admin"){
+    print("enter password:\n")
+    b=readLine()!
+    if(b=="88888888"){
+        print("welcome\n")
+    }
+    else{
+        print("wrong password\n")
+    }
+}
+else{
+    print("username doesnt exist\n")
+}
+print("889")
+
+
+
+
