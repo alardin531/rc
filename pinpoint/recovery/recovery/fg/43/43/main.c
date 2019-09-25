d3e352ca353810141f5cc6b1d001915b057d39d2
s20181102934 <s20181102934@admindeiMac-38.local>
2019-09-25 10:27:31 +0800

    sfs  game

--- pinpoint/recovery/recovery/fg/43/43/main.c
+++ pinpoint/recovery/recovery/fg/43/43/main.c
@@ -10,11 +10,36 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int a=9;
-    int *p;
-    p=&a;
-    printf("%d\n",p);
-    printf("%x\n",a);
-    printf("ds");
-    return 0;
+    int a=2222;
+    int b;
+    printf("输入你的猜想:\n");
+    scanf("%d",&b);
+    int c=b-a;
+    if (b<=a) {
+        c=-c;
+    }
+    while (b!=a) {
+        if(c>1000){
+            printf("你在想什么啊？？？\n");
+        }
+        else if(c>100){
+            printf("离正确答案比较远\n");
+        }
+        else if(c>10){
+            printf("快猜对了，还差一些\n");
+        }
+        else if(c>=1){
+            printf("就快得到正确结果了！\n");
+        }
+        else{
+            printf("too far\n");
+        }
+        scanf("%d",&b);
+        c=b-a;
+        if (b<=a) {
+            c=-c;
+        }
+    }
+    printf("真不容易啊猜对了！！！\n");
+    
 }
